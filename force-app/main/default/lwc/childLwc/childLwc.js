feb602c8593efe7be04a2b2ffc73918b4381bbdc
Chetan0602 <95068201+Chetan0602@users.noreply.github.com>
2023-07-12 15:59:34 +0530

    Changes Done In Child LWC

--- force-app/main/default/lwc/childLwc/childLwc.js
+++ force-app/main/default/lwc/childLwc/childLwc.js
@@ -1,11 +1,11 @@
-import { LightningElement, api } from 'lwc';
-
+import {LightningElement,api} from 'lwc';
+// Test
 export default class ChildLwc extends LightningElement {
     @api get getidfromparent() {
         return this.getidfromparent;
-    } 
+    }
     set getidfromparent(val) {
-        this.data1=val;
+        this.data1 = val;
     }
     data1;
 }
